e8be55df3f51f9ce4ccf3121538f7411cc7acf50
VanBiak <s281867@ocsb.local>
2017-10-17 14:06:39 -0400

    Times Table

--- src/TimesTableChallenge.java
+++ src/TimesTableChallenge.java
@@ -1,56 +1,47 @@
 import java.util.Scanner;
-import java.util.Random;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class TimesTableChallenge {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in=new Scanner (System.in);
-		Random rnd=new Random();
 
 		//Setup Variables
 		int hi;
 		int low;
 		int num;
-		int score=0;
-		int x=1;
 		//Ask for hi, low and num
 		System.out.println("Welcome to the Times Table Challenge!");
 		System.out.println("");
-		System.out.println("What is the biggest number you want?");
+		System.out.println("High number?");
 		hi=in.nextInt();
 		in.nextLine();
-		System.out.println("What is the smaller number you want?");
+		System.out.println("Smaller number?");
 		low=in.nextInt();
 		in.nextLine();
 
-		//Loop and ask the questions
-		while (score<10) {
-			double num1=rnd.nextInt(hi-low+1)+low;
-			double num2=rnd.nextInt(hi-low+1)+low;
-			System.out.println("Question "+x+") "+num1+" / "+num2+" = ?");
-			int ans=in.nextInt();
-			if (ans==num1/num2) {
-				score++;
-				x++;
-				System.out.println("Right!");
-
-			}else {
-				System.out.println("Wrong! The answer is "+(num1/num2));
-				score=0;
+		if (hi<low) {
+			num=hi;
+			hi=low;
+			low=num;
+		} 
+			for (int i=low; i<=hi;i++) {
+				System.out.print("\t"+i );
 			}
+			for (int x=low; x<=hi;x++) {
+				System.out.println("\t");
+				System.out.print(""+x+"\t");
+
+				for (int y=low; y<=hi;y++) {
+					System.out.print(y*x+"\t");
+				}
+				System.out.println();
+			}
+			//Print Results
+
 		}
-		//Print Results
-		
-		if (score==10) {
-			double num1=rnd.nextInt(hi-low+1)+low;
-			double num2=rnd.nextInt(hi-low+1)+low;
-			double num3=num1+num2;
-			System.out.println("BONUS: "+num1+" + "+num3+" - "+num2+" x "+num1*num2+" = ?");
-			in.nextInt();
-			double answer;
-			
-		}
+
 
 	}
-
-}
